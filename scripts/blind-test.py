d7a785c01aed37bc30edfb1e881367f0ccc093ac
1emvr <1emvr@protonmail.com>
2024-09-14 03:35:35 -0400

    uhhhh...

---
+++ scripts/blind-test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+1emvr@protonmail.com
+lab uses TrackingId cookie vulnerable to blind-boolean sqli
+
+database_version: postgresql
+parameter_type: string
+column_count: 1
+
+""
